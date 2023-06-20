988a68de8576fd493954690718f3321ce2aef31a
SoerenP <stivhobbykniv@gmail.com>
2023-06-20 20:53:59 +0200

    test

--- MacaroonCore/FirstPartyCaveat.cs
+++ MacaroonCore/FirstPartyCaveat.cs
@@ -2,35 +2,35 @@
 
 namespace MacaroonCore
 {
-	public class FirstPartyCaveat : Caveat
-	{
-		public static string FirstPartyCaveatIndicator = "0";
+    public class FirstPartyCaveat : Caveat
+    {
+        public static string FirstPartyCaveatIndicator = "0";
 
-		public static bool VerificationIdIndicatesFirstPartyCaveat(string verificationId)
-		{
-			return FirstPartyCaveatIndicator.Equals(verificationId, System.StringComparison.Ordinal);
-		}
+        public static bool VerificationIdIndicatesFirstPartyCaveat(string verificationId)
+        {
+            return FirstPartyCaveatIndicator.Equals(verificationId, System.StringComparison.Ordinal);
+        }
 
-		internal FirstPartyCaveat()
-		{
+        internal FirstPartyCaveat()
+        {
 
-		}
+        }
 
-		public FirstPartyCaveat(string predicate, string location = null)
-		{
-			VerificationId = FirstPartyCaveatIndicator;
+        public FirstPartyCaveat(string predicate, string location = null)
+        {
+            VerificationId = FirstPartyCaveatIndicator;
 
-			Location = location;
+            Location = location;
 
-			//First class caveats has the predicate as their Id.
-			CaveatId = predicate;
-		}
+            //First class caveats has the predicate as their Id.
+            CaveatId = predicate;
+        }
 
-		public override bool IsFirstPartyCaveat { get { return true; } }
-		
-		public override byte[] Payload() =>
-								Encode.DefaultStringDecoder(CaveatId)
-								.Concat(Encode.DefaultStringDecoder(VerificationId))
-								.ToArray();
-	}
+        public override bool IsFirstPartyCaveat { get { return true; } }
+
+        public override byte[] Payload() =>
+                                Encode.DefaultStringDecoder(CaveatId)
+                                .Concat(Encode.DefaultStringDecoder(VerificationId))
+                                .ToArray();
+    }
 }
